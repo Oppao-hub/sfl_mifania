e9ea4b594de84227ba3d12b9f496bfad55f38eea
Bien <lopao0012@gmail.com>
2026-05-05 20:42:48 +0800

    Add onDelete cascade option to customer join column in Redemption entity

--- src/Entity/Redemption.php
+++ src/Entity/Redemption.php
@@ -14,6 +14,7 @@ class Redemption
     private ?int $id = null;
 
     #[ORM\ManyToOne(inversedBy: 'redemptions')]
+    #[ORM\JoinColumn(onDelete: 'CASCADE')]
     private ?Customer $customer = null;
 
     #[ORM\ManyToOne(inversedBy: 'redemptions', targetEntity: Reward::class)]
