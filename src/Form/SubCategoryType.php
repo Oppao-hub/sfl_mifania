545dc8a827f0ce2d1472073c8dd21b3a0093e015
Bien <lopao0012@gmail.com>
2026-05-05 20:46:39 +0800

    Update SubCategoryType form: add slug and icon fields with placeholders and help text

--- src/Form/SubCategoryType.php
+++ src/Form/SubCategoryType.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use App\Entity\SubCategory;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,20 @@ class SubCategoryType extends AbstractType
         $builder
             ->add('name')
             ->add('description')
-            ->add('slug')
+            ->add('slug', TextType::class, [
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Automatically generated if left blank'
+                ]
+            ])
+            ->add('icon', TextType::class, [
+                'required' => false,
+                'label' => 'Icon Identifier',
+                'attr' => [
+                    'placeholder' => 'e.g., lucide:leaf, material-symbols:category'
+                ],
+                'help' => 'Use Iconify format (set:name). See ux-icons documentation.'
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
                 'choice_label' => 'name',
