90cc497e5ada76b032d8671cc07cec8000a0d752
Bien <lopao0012@gmail.com>
2026-05-22 09:29:29 +0800

    update notifications when someon log in

--- src/EventSubscriber/LoginLogoutSubscriber.php
+++ src/EventSubscriber/LoginLogoutSubscriber.php
@@ -2,16 +2,21 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
 use App\Service\ActivityLogger;
+use App\Service\NotificationPublisher;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
-use App\Entity\User;
 
 class LoginLogoutSubscriber implements EventSubscriberInterface
 {
-    public function __construct(private ActivityLogger $logger)
-    {
+    public function __construct(
+        private ActivityLogger $logger,
+        private NotificationPublisher $notificationPublisher,
+        private UserRepository $userRepository
+    ) {
     }
 
     public static function getSubscribedEvents(): array
@@ -37,10 +42,41 @@ class LoginLogoutSubscriber implements EventSubscriberInterface
                 $loginMethod = 'Google OAuth';
             }
 
+            // 1. Notify the User themselves (Security Alert)
+            $this->notificationPublisher->send(
+                $user,
+                'Security Alert: New Login',
+                "A new login was detected on your account via {$loginMethod}.",
+                'app_account', // Link to account settings
+                [],
+                'security',
+                false // Don't flush yet
+            );
+
+            // 2. Notify Management (Admins & Staff)
+            $management = $this->userRepository->findAllManagement();
+            foreach ($management as $manager) {
+                if ($manager->getId() === $user->getId()) {
+                    continue;
+                }
+
+                $this->notificationPublisher->send(
+                    $manager,
+                    'User Login Detected',
+                    "User {$user->getEmail()} has logged into the system.",
+                    'app_dashboard',
+                    [],
+                    'system',
+                    false // Don't flush yet
+                );
+            }
+
+            // 3. Log the Activity AND Flush everything (Log + Notifications)
             $this->logger->log(
                 'LOGIN',
                 "User {$user->getUserIdentifier()} logged in via {$loginMethod}.",
-                $user
+                $user,
+                true // Perform Flush here to save everything at once
             );
         }
     }
