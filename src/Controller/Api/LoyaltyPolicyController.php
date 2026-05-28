fac3436e96699872a3e57342f8869b4543695957
Bien <lopao0012@gmail.com>
2026-05-28 22:15:50 +0800

    refactor: simplify LoyaltyPolicyController by delegating loyalty point calculations to RewardManager, enhancing code maintainability

--- src/Controller/Api/LoyaltyPolicyController.php
+++ src/Controller/Api/LoyaltyPolicyController.php
@@ -7,27 +7,22 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use App\Entity\User;
+use App\Service\RewardManager;
 
 #[Route('/api/loyalty-policy')]
 class LoyaltyPolicyController extends AbstractController
 {
-    public function __construct(
-        private readonly int $loyaltyPointsPerCurrency,
-        private readonly float $loyaltyMinOrderForRedemption,
-        private readonly float $loyaltyMaxRedemptionPercentage
-    ) {}
-
     #[Route('', name: 'api_loyalty_policy_get', methods: ['GET'])]
-    public function index(#[CurrentUser] ?User $user): JsonResponse
+    public function index(#[CurrentUser] ?User $user, RewardManager $rewardManager): JsonResponse
     {
         if (!$user) {
             return $this->json(['error' => 'Unauthorized'], 401);
         }
 
         return $this->json([
-            'pointsPerCurrency' => max(1, $this->loyaltyPointsPerCurrency),
-            'minOrderForRedemption' => (float) $this->loyaltyMinOrderForRedemption,
-            'maxRedemptionPercentage' => (float) $this->loyaltyMaxRedemptionPercentage,
+            'pointsPerCurrency' => $rewardManager->getPointsPerCurrency(),
+            'minOrderForRedemption' => $rewardManager->getMinOrderForRedemption(),
+            'maxRedemptionPercentage' => $rewardManager->getMaxRedemptionPercentage(),
         ]);
     }
 }
